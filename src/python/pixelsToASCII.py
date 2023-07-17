68a9d16b78b48834bcddff324627602b0571b889
Leandro Assis dos Santos <assissantosleandro@poli.ufrj.br>
2023-07-17 20:13:57 -0300

    ASCII OK

--- src/python/pixelsToASCII.py
+++ src/python/pixelsToASCII.py
@@ -1,31 +1,40 @@
 from PIL import Image
+import numpy as np
 
-ASCII_CHARS = '@%#*+=-:. '
+ASCII_CHARS = '@%#*$&[]}{^+=-:. '
 
-def map_pixels_to_ascii(image, range_width=25):
+# recebe uma imagem do PIL e retorna uma string com os caracteres ASCII
+def map_pixels_to_ascii(image : Image) -> str:
     pixels = image.getdata()
     ascii_str = ''
+
     for pixel_value in pixels:
-        ascii_str += ASCII_CHARS[pixel_value // range_width]
+        gray = int(0.299*pixel_value[0] + 0.587*pixel_value[1] + 0.114*pixel_value[2]) # calcula o valor de cinza do pixel
+        ascii_str += ASCII_CHARS[gray // len(ASCII_CHARS)] # mapeia o valor de cinza para um caractere ASCII
     return ascii_str
 
-def convert_image_to_ascii(image):
-    ascii_str = map_pixels_to_ascii(image)
-    img_width = image.width
-    ascii_str_len = len(ascii_str)
-    ascii_img = ''
-    for i in range(0, ascii_str_len, img_width):
-        ascii_img += ascii_str[i:i+img_width] + '\n'
-    return ascii_img
-
-def main(image_path):
+# recebe uma imagem como np.array e converte para ascii, salvando no arquivo apontado pelo path
+def convert_image_to_ascii(image : np.ndarray, output_path : str) -> int:
     try:
-        image = Image.open(image_path)
-    except Exception as e:
-        print(e)
-        return
-    ascii_img = convert_image_to_ascii(image)
-    print(ascii_img)
+        pil_image = Image.fromarray(image) # converte o np.array para imagem do PIL
 
-# Exemplo de uso:
-main('caminho_da_imagem.jpg')
+        ascii_str = map_pixels_to_ascii(pil_image) # converte a imagem para ascii
+        
+        img_width = pil_image.width # largura da imagem
+        ascii_str_len = len(ascii_str) # tamanho da string ascii
+        
+        with open('./out/'+output_path, 'w') as f:
+            for i in range(0, ascii_str_len, img_width):
+                f.write(ascii_str[i:i+img_width] + '\n')
+    except Exception as e:
+        print(f'Erro: {e}.')
+        print('Erro ao converter a imagem para ASCII.')
+        return 0
+
+    return 1
+
+if __name__ == '__main__':
+    slice56 = Image.open('./sample/image_weighted_average_byhand.png')
+    slice56 = np.array(slice56)
+
+    print(convert_image_to_ascii(slice56, 'slice56.txt'))
